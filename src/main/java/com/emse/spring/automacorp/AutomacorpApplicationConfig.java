1a27724f5bf269f7b417039f028e08deb52a8ff6
cmarquet <cormarquet@gmail.com>
2024-10-17 22:51:35 +0200

    4

--- src/main/java/com/emse/spring/automacorp/AutomacorpApplicationConfig.java
+++ src/main/java/com/emse/spring/automacorp/AutomacorpApplicationConfig.java
@@ -1,4 +1,17 @@
 package com.emse.spring.automacorp;
 
+import com.emse.spring.automacorp.hello.GreetingService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.CommandLineRunner;
+
+@Configuration
 public class AutomacorpApplicationConfig {
+
+    @Bean
+    public CommandLineRunner greetingCommandLine(GreetingService greetingService) {
+        return args -> {
+            greetingService.greet("Spring");
+        };
+    }
 }
