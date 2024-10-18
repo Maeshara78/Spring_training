d7dccb6eb1d4820fd83accb9134458b66ab50fbd
cmarquet <cormarquet@gmail.com>
2024-10-18 15:43:14 +0200

    WindowDaoCustom.java

--- src/test/java/com/emse/spring/automacorp/dao/WindowDaoTest.java
+++ src/test/java/com/emse/spring/automacorp/dao/WindowDaoTest.java
@@ -3,11 +3,14 @@ package com.emse.spring.automacorp.dao;
 import com.emse.spring.automacorp.dao.WindowDao;
 import com.emse.spring.automacorp.model.WindowEntity;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
 
 @DataJpaTest // (1)
 class WindowDaoTest {
@@ -19,5 +22,20 @@ class WindowDaoTest {
         WindowEntity window = windowDao.getReferenceById(-10L); // (3)
         Assertions.assertThat(window.getName()).isEqualTo("Window 1");
         Assertions.assertThat(window.getWindowStatus().getValue()).isEqualTo(1.0);
+    }
+
+    @Test
+    public void shouldFindRoomsWithOpenWindows() {
+        List<WindowEntity> result = windowDao.findRoomsWithOpenWindows(-10L);
+        Assertions.assertThat(result)
+                .hasSize(1)
+                .extracting("id", "name")
+                .containsExactly(Tuple.tuple(-10L, "Window 1"));
+    }
+
+    @Test
+    public void shouldNotFindRoomsWithOpenWindows() {
+        List<WindowEntity> result = windowDao.findRoomsWithOpenWindows(-9L);
+        Assertions.assertThat(result).isEmpty();
     }
 }
