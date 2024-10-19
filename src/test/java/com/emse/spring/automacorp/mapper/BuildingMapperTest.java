a2fd23c8793e5bfd4a7f6bef4cb5aaaf518cba47
cmarquet <cormarquet@gmail.com>
2024-10-19 22:52:18 +0200

    BuildingMapperTest

---
+++ src/test/java/com/emse/spring/automacorp/mapper/BuildingMapperTest.java
@@ -0,0 +1,104 @@
+package com.emse.spring.automacorp.mapper;
+
+import com.emse.spring.automacorp.model.BuildingEntity;
+import com.emse.spring.automacorp.model.FakeEntityBuilder;
+import com.emse.spring.automacorp.record.BuildingRecord;
+import com.emse.spring.automacorp.record.HeaterRecord;
+import com.emse.spring.automacorp.record.RoomRecord;
+import com.emse.spring.automacorp.record.WindowRecord;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class BuildingMapperTest {
+
+    @Test
+    void shouldMapBuilding() {
+        // Arrange
+        BuildingEntity buildingEntity = FakeEntityBuilder.createBuildingEntity(1L, "Building");
+
+        // Act
+        BuildingRecord building = BuildingMapper.of(buildingEntity);
+
+        // Assert
+        BuildingRecord expectedBuilding = new BuildingRecord(
+                1L,
+                "Building",
+                23.2,
+                List.of(
+                    new RoomRecord(
+                        11L,
+                        1,
+                        "Room1Building",
+                        23.2,
+                        26.4,
+                        List.of(
+                            new WindowRecord(
+                                111L,
+                                "Window1Room1Building",
+                                0.0,
+                                11L
+                            ),
+                            new WindowRecord(
+                                112L,
+                                "Window2Room1Building",
+                                0.0,
+                                11L
+                            )
+                        ),
+                        List.of(
+                            new HeaterRecord(
+                                111L,
+                                "Heater1Room1Building",
+                                11L,
+                                0.0
+                            ),
+                            new HeaterRecord(
+                                112L,
+                                "Heater2Room1Building",
+                                11L,
+                                0.0
+                            )
+                        ),
+                        1L
+                    ),
+                    new RoomRecord(
+                        12L,
+                        1,
+                        "Room2Building",
+                        23.2,
+                        26.4,
+                        List.of(
+                            new WindowRecord(
+                                121L,
+                                "Window1Room2Building",
+                                0.0,
+                                12L
+                            ),
+                            new WindowRecord(
+                                122L,
+                                "Window2Room2Building",
+                                0.0,
+                                12L
+                            )
+                        ),
+                        List.of(
+                            new HeaterRecord(
+                                121L,
+                                "Heater1Room2Building",
+                                12L,
+                                0.0
+                            ),
+                            new HeaterRecord(
+                                122L,
+                                "Heater2Room2Building",
+                                12L,
+                                0.0
+                            )
+                        ),
+                        1L
+                    )
+                )
+        );
+    }
+}
--- src/test/java/com/emse/spring/automacorp/mapper/WindowMapperTest.java
+++ src/test/java/com/emse/spring/automacorp/mapper/WindowMapperTest.java
@@ -1,4 +1,33 @@
 package com.emse.spring.automacorp.mapper;
 
+import com.emse.spring.automacorp.model.FakeEntityBuilder;
+import com.emse.spring.automacorp.model.SensorEntity;
+import com.emse.spring.automacorp.model.WindowEntity;
+import com.emse.spring.automacorp.record.WindowRecord;
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+
 public class WindowMapperTest {
+
+    @Test
+    void shouldMapWindow() {
+        // Arrange
+        WindowEntity windowEntity = FakeEntityBuilder.createBuildingEntity(1L, "Building")
+                .getRooms()
+                .stream()
+                .flatMap(room -> room.getWindows().stream())
+                .min(Comparator.comparing(WindowEntity::getName))
+                .orElseThrow(IllegalArgumentException::new);
+        // Act
+        WindowRecord window = WindowMapper.of(windowEntity);
+
+        // Assert
+        WindowRecord expectedWindow = new WindowRecord(
+                111L,
+                "Window1Room1Building",
+                0.0,
+                11L
+        );
+    }
 }
